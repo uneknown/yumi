2c9054f7672abe269da33e8d7730fc576b09dbd5
DigitalGreyHat <90456651+DigitalGreyHat@users.noreply.github.com>
2022-10-01 13:57:54 +0200

    feat: add image generator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import ColorHash from "color-hash";
 
 const app = new Hono();
 
@@ -12,9 +13,40 @@ app.get('/:name', async (c) => {
 	const size = parseSize(c.req.query('size'));
 
 	const hash = await hashString(name);
+	const image = generateSVG(hash, size);
 
-	return c.json({ hash, size }, 200);
+	return c.html(image, 200);
 });
+
+export const generateColors = (s: string): [string, string] => {
+	const colorHash = new ColorHash();
+	const s1 = s.substring(0, s.length / 2);
+	const s2 = s.substring(s.length / 2);
+	const c1 = colorHash.hex(s1);
+	const c2 = colorHash.hex(s)
+  
+	return [c1, c2];
+  };
+
+const generateSVG = (hash: string, size: number) => {
+	const [c1, c2] = generateColors(hash);
+
+	const svg = `
+  <svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="${size / 2}" cy="${size / 2}" r="${
+	  size / 2
+	}" fill="url(#gradient)" />
+	<defs>
+	  <linearGradient id="gradient" x1="0" y1="0" x2="${size}" y2="${size}" gradientUnits="userSpaceOnUse">
+		<stop stop-color="${c1}" />
+		<stop offset="1" stop-color="${c2}" />
+	  </linearGradient>
+	</defs>
+  </svg>
+	`.trim();
+
+	return svg;
+}
 
 const hashString = async (data: string) => {
 	const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(data));
